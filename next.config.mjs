8270cb87540b324638a0ff7937e45dcd8f7c7d02
tejasinfotech <jobjunction4u1981@gmail.com>
2026-02-22 18:25:23 +0530

    chnahes

--- next.config.mjs
+++ next.config.mjs
@@ -4,18 +4,22 @@
 // };
 
 // export default nextConfig;
-
 import nextPWA from "next-pwa";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
 
 const withPWA = nextPWA({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development", // works automatically
-});
 
-const nextConfig = {
-  reactStrictMode: true,
-};
+  // 🔥 VERY IMPORTANT
+  disable: process.env.NODE_ENV === "development",
+
+  buildExcludes: [/middleware-manifest.json$/],
+});
 
 export default withPWA(nextConfig);
