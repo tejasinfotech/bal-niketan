6f84fcfeb181591e12298bf964c6967270e658eb
tejasinfotech <jobjunction4u1981@gmail.com>
2026-03-28 21:41:38 +0530

    change

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,37 +1,22 @@
 export default function sitemap() {
-  const baseUrl = "hhttps://www.balniketan.ind.in"; // 🔴 replace with your domain
+  const baseUrl = "https://www.balniketan.ind.in"; // change to your domain
 
-  return [
-    {
-      url: `${baseUrl}/`,
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 1,
-    },
-    {
-      url: `${baseUrl}/about`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-        {
-      url: `${baseUrl}/gallery`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },   
-    {
-      url: `${baseUrl}/facilities`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-
-    {
-      url: `${baseUrl}/contact`,
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.7,
-    },
+  const routes = [
+    "",
+    "/about",
+    "/activities",
+    "/contact",
+    "/facilities",
+    "/faculty",
+    "/gallery",
   ];
+
+  const staticPages = routes.map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date(),
+    changeFrequency: "weekly",
+    priority: route === "" ? 1 : 0.8,
+  }));
+
+  return staticPages;
 }
